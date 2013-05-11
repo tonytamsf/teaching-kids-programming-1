3f5351e4bb7b517e5f78f277a3b6c67f14d2f5b7
Tony Tam <tonytam@yahoo.com>
2013-05-11 16:55:07 -0700

    cloning from https://code.google.com/p/teaching-kids-programming/

--- social-drawing/social-drawing.js
+++ social-drawing/social-drawing.js
@@ -1,3 +1,4 @@
+
 function util_load_css(event)
 {
     var headID = document.getElementsByTagName("head")[0];         
@@ -13,10 +14,10 @@ function util_load_css(event)
     // Set the label to reflect our current palette
     var palette_label = document.getElementById("palette-label");
     if (palette_label) {
-﻿  palette_label.innerHTML = this.id;
-﻿  palette_label.style.color = currentStrokeStyle;
-﻿  //DEBUG: alert(currentStrokeStyle);
-﻿  //DEBUG: alert(this.id);
+	palette_label.innerHTML = this.id;
+	palette_label.style.color = currentStrokeStyle;
+	//DEBUG: alert(currentStrokeStyle);
+	//DEBUG: alert(this.id);
     }
 }
 
@@ -25,10 +26,10 @@ function util_refresh_state() {
     // Set the label to reflect our current palette
     var palette_label = document.getElementById("palette-label");
     if (palette_label) {
-﻿  palette_label.style.color = currentStrokeStyle;
+	palette_label.style.color = currentStrokeStyle;
     }
     var pen_label = document.getElementById("pen");
     if (pen_label) {
-﻿  pen_label.style.color = currentStrokeStyle;
+	pen_label.style.color = currentStrokeStyle;
     }
 }
